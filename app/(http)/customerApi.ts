0d3b437527c0d28ab1d8827aeda3b4cbae25bc21
moon <cksdnd004@naver.com>
2023-08-23 23:07:58 +0900

    slug page developing 30%

--- app/(http)/customerApi.ts
+++ app/(http)/customerApi.ts
@@ -3,10 +3,16 @@ import http from "./http";
 
 export const getNotices = async () => {
   try {
-    const fetchNotice = await http.get("/notice/list");
-    const noticeData = await fetchNotice.data;
-    return noticeData;
+    const fetchNotices = await http.get("/notice/list");
+    const noticesData = await fetchNotices.data;
+    return noticesData;
   } catch (error) {
     console.error("getNotices[error]: ", error);
   }
+};
+
+export const getNotice = async (params) => {
+  try {
+    const fetchNotice = await http.get("/notice/view", { params: params });
+  } catch (error) {}
 };
---
+++ app/(page)/(baseLayout)/customer/[slug]/page.tsx
@@ -0,0 +1,17 @@
+//@ts-nocheck
+import React from "react";
+import { getNotices } from "@/app/(http)/customerApi";
+
+async function generateStaticParams() {
+  const noticeData = await getNotices();
+
+  return noticeData.map((notice) => ({
+    slug: notice.noticeList.content.idx,
+  }));
+}
+
+const page = ({ params }: { params: { slug: string } }) => {
+  return <div>공지사항!!</div>;
+};
+
+export default page;
