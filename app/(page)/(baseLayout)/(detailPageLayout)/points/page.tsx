95c09d4ea8bf101c31aae5972699211647290431
Jay.J <jayj.fe@gmail.com>
2023-10-29 14:02:58 +0900

    point 페이지 데이터호출

--- app/(page)/(baseLayout)/(detailPageLayout)/points/page.tsx
+++ app/(page)/(baseLayout)/(detailPageLayout)/points/page.tsx
@@ -5,17 +5,31 @@ import Image from "next/image";
 import DetailPageNav from "@/app/(Components)/Nav/DetailPageNav";
 
 import { getPointShopHome } from "@/app/(http)/apis/detailApi";
+import { URL } from "@/app/(util)/CATEGORY";
 import "../../../../../styles/pages/hotDeal.scss";
 
-async function getData() {
-  // const hotDealDetailData = await getDetailList(URL.HOTDEAL, params);
-  const getPointshopData = await getPointShopHome();
+async function getData(searchParams) {
+  let params = {
+    cpage: searchParams.cpage || 1,
+    categCd: searchParams.categCd || null,
+  };
+  const pointDetailData = await getPointShopHome(URL.POINT, params);
 
-  return { subCategories: getPointshopData.categ1List };
+  console.log(pointDetailData.pointshopList);
+  // return {
+  //   subCategories: pointDetailData.categ1List,
+  // }
+  return {
+    subCategories: pointDetailData.categ1List,
+    itemListPsConv: pointDetailData.pointshopList.PS_CONV,
+    itemListPsFood: pointDetailData.pointshopList.PS_FOOD,
+    itemListPsProduct: pointDetailData.pointshopList.PS_PRODUCT,
+    
+  };
 }
 
-const page = async () => {
-  const data = await getData();
+const page = async ({ searchParams }) => {
+  const data = await getData(searchParams);
   return (
     <>
       <DetailPageNav navList={data.subCategories} />
@@ -31,7 +45,15 @@ const page = async () => {
             />
           </h2>
         </header>
-        <article className="hotdeal-list-con"></article>
+        <article className="hotdeal-list-con">
+          <ul>
+            <li>
+              {data.itemListPsFood.map((item)=>{
+                return <div key={item.brandId}>{item.brandName}</div>
+              })}
+            </li>
+          </ul>
+        </article>
       </main>
     </>
   );
