121b608f67e120f30fc2247e365f6cf68f8053d7
Jay.J <jayj.fe@gmail.com>
2024-01-24 06:35:40 +0900

    hotdeal Detail Page under development

--- app/(page)/(baseLayout)/(detailPageLayout)/hotdeal/[slug]/page.tsx
+++ app/(page)/(baseLayout)/(detailPageLayout)/hotdeal/[slug]/page.tsx
@@ -1,9 +1,13 @@
 //@ts-nocheck
+// "use client";
+// import React, { useState } from "react";
+import React from "react";
 import { getCategories, getHotdealItem } from "@/app/(http)/apis/detailApi";
 import { CATE_LABEL } from "@/app/(util)/CATEGORY";
-import React from "react";
 import DetailPageNav from "@/app/(Components)/Nav/DetailPageNav";
 import ImageWithFallback from "@/app/(Components)/ImageWithFallback";
+
+import "../../../../../../styles/components/hotDealDetail.scss";
 
 async function getData(productNum) {
   const [hotdealItem] = await Promise.all([getHotdealItem(productNum)]);
@@ -12,13 +16,18 @@ async function getData(productNum) {
 }
 
 const page = async ({ params }: { params: { slug: string } }) => {
+  // const [showPrdInfomation, setShowPrdInfomation] = useState(false); // product moreInfomation
   const data = await getData(params.slug);
+  console.log(data.hotdealItem);
+  
+  if(data.hotdealItem === null){
+    data.hotdealItem.categ1List = [];
+  }
 
   return (
-
     <>
       <DetailPageNav navList={data.hotdealItem.categ1List} />
-      <section>
+      <section className="hotdeal-detail">
         <figure>
           <ImageWithFallback
             src={data.hotdealItem.hotdealDTO?.image}
@@ -30,25 +39,53 @@ const page = async ({ params }: { params: { slug: string } }) => {
         </figure>
         <header>
           <h2>{data.hotdealItem.hotdealDTO?.name}</h2>
-          <div>
+          <div className="price-box">
             <div>
-              <p>{data.hotdealItem.hotdealDTO?.sale}%</p>
-              <p>{data.hotdealItem.hotdealDTO?.price}원</p>
+              {data.hotdealItem.hotdealDTO?.sale && (
+                <p className="percent"><em>{data.hotdealItem.hotdealDTO?.sale}</em>%</p>
+              )}
+              <p className="price-current"><em>{data.hotdealItem.hotdealDTO?.price}</em>원</p>
             </div>
-            <p>{data.hotdealItem.hotdealDTO?.listPrice}원</p>
+            {data.hotdealItem.hotdealDTO?.listPrice && (
+              <p className="price-org"><del>{data.hotdealItem.hotdealDTO.listPrice}</del>원</p>
+            )}
           </div>
         </header>
-        <button>구매하기</button>
-        <article>
+        <button className="btn-primary">구매하기</button>
+        <article className="prd-detail">
           <h3>상세정보</h3>
           <div>
-            <h4>상품안내</h4>
-            <h4>상품상세정보</h4>
+            {/* <h4>상품안내</h4>
+            <h4>상품상세정보</h4> */}
             <div
               dangerouslySetInnerHTML={{
                 __html: data.hotdealItem.hotdealDTO?.detailInfo,
               }}
+              className="prd-img hidden"
             ></div>
+            <div class="btn_wrap">
+              <button 
+                type="button"
+                >
+                  자세히보기
+                </button>
+            </div>
+            {/* <div
+              dangerouslySetInnerHTML={{
+                __html: data.hotdealItem.hotdealDTO?.detailInfo,
+              }}
+              className={"prd-img" + (showPrdInfomation ? '' : ' hidden') }
+            ></div>
+            {showPrdInfomation && (
+              <div class="btn_wrap">
+                <button 
+                  type="button"
+                  onClick={setShowPrdInfomation(true)}
+                  >
+                    자세히보기
+                  </button>
+              </div>
+            )} */}
           </div>
         </article>
       </section>
